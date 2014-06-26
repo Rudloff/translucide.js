38869e2e32e1abf2aec37b05a1fa3712c4b7f526
Pierre Rudloff <contact@rudloff.pro>
2014-06-27 00:59:25 +0200

    Fetch all results

--- translucide.js
+++ translucide.js
@@ -3,9 +3,50 @@
 var casper = require('casper').create();
 var utils = require('utils');
 var system = require('system');
+var page = 1;
 
 function getTD(casper, i, j) {
     return casper.getElementInfo('.section-content tbody tr:nth-child(' + (i + 1) + ') td:nth-child(' + j + ')').text;
+}
+
+function nextPage() {
+    var results = this.getElementsInfo('.section-content tbody tr'), i;
+    for (i = 0; i < results.length; i += 1) {
+        this.echo(getTD(this, i, 1) + ' | ' + getTD(this, i, 2) + ' | ' + this.getElementInfo('.section-content tbody tr:nth-child(' + (i + 1) + ') td:nth-child(3) .withJS').text.replace(/(\r\n|\n|\r)/gm, '') + ' | ' + getTD(this, i, 4) + ' | ' + getTD(this, i, 5) + ' | ' + getTD(this, i, 6));
+    }
+    if (this.exists('.btn-next:enabled')) {
+        this.click('.btn-next');
+        page += 1;
+        this.waitForSelector('.btn-page input[value="' + page + '"]:disabled', nextPage);
+    }
+}
+
+function clickCheckBoxes() {
+    var fieldset, results, i;
+    this.echo('Fetching doctors/students: page ' + page);
+    if (casper.cli.args[1] === 'student') {
+        fieldset = 'fieldset:nth-of-type(2)';
+    } else {
+        fieldset = 'fieldset:first-of-type';
+    }
+    if (this.exists('#j_idt74 ' + fieldset + ' .section-recipient tbody tr')) {
+        results = this.getElementsInfo('#j_idt74 ' + fieldset + ' .section-recipient tbody tr');
+        for (i = 1; i <= results.length; i += 1) {
+            this.click(fieldset + ' tr:nth-child(' + i + ') input[type="checkbox"]');
+        }
+        if (this.exists(fieldset + ' .btn-next:enabled')) {
+            this.click(fieldset + ' .btn-next');
+            page += 1;
+            this.waitForSelector(fieldset + ' .btn-page input[value="' + page + '"]:disabled', clickCheckBoxes);
+        } else {
+            this.click('input[name="j_idt74:j_idt194"]');
+            page = 1;
+            this.echo('Fetching results…');
+        }
+    } else {
+        this.echo('No result');
+        this.exit();
+    }
 }
 
 if (casper.cli.args[0]) {
@@ -20,31 +61,9 @@ if (casper.cli.args[0]) {
         this.click('input[name="j_idt62:j_idt70"]');
     });
 
-    casper.waitForSelector('#j_idt74', function () {
-        var fieldset, results, i;
-        if (casper.cli.args[1] === 'student') {
-            fieldset = 'fieldset:nth-of-type(2)';
-        } else {
-            fieldset = 'fieldset:first-of-type';
-        }
-        if (this.exists('#j_idt74 ' + fieldset + ' .section-recipient tbody tr')) {
-            results = this.getElementsInfo('#j_idt74 ' + fieldset + ' .section-recipient tbody tr');
-            for (i = 1; i <= results.length; i += 1) {
-                this.click(fieldset + ' tr:nth-child(' + i + ') input[type="checkbox"]');
-            }
-            this.click('input[name="j_idt74:j_idt194"]');
-        } else {
-            this.echo('No result');
-            this.exit();
-        }
-    });
+    casper.waitForSelector('#j_idt74', clickCheckBoxes);
 
-    casper.waitForSelector('table[id="j_idt17:dataTable"]', function () {
-        var results = this.getElementsInfo('.section-content tbody tr'), i;
-        for (i = 0; i < results.length; i += 1) {
-            this.echo(getTD(this, i, 1) + ' | ' + getTD(this, i, 2) + ' | ' + this.getElementInfo('.section-content tbody tr:nth-child(' + (i + 1) + ') td:nth-child(3) .withJS').text.replace(/(\r\n|\n|\r)/gm, '') + ' | ' + getTD(this, i, 4) + ' | ' + getTD(this, i, 5) + ' | ' + getTD(this, i, 6));
-        }
-    });
+    casper.waitForSelector('table[id="j_idt17:dataTable"]', nextPage);
     casper.run();
 } else {
     casper.echo('Usage: casperjs ' + system.args[3] + ' DoctorName [student]');
